417d7b9a45f747fbbfa88b75e1afb32dffd6a83d
omarzydan610 <omarzydan610@gmail.com>
2024-12-05 16:03:02 +0200

    initialize repo

---
+++ instamail-backend/src/main/java/com/example/instamail_backend/InstamailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.instamail_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstamailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstamailBackendApplication.class, args);
+	}
+
+}
