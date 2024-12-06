0aabc9b840e86d756589b823993ad951a1cae81e
omarzydan610 <omarzydan610@gmail.com>
2024-12-06 17:32:23 +0200

    add routes and create home screen

--- instamail-frontend/src/App.jsx
+++ instamail-frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import Signup from "./pages/signup";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <Signup />;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/register" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
